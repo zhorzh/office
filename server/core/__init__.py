19cdc973edef1a6375f39c4e450b4bb36d090ba8
zhorzh <zhorzh.alexandr@gmail.com>
2015-09-23 19:58:48 -0400

    setup(project): add postgres config

--- server/core/__init__.py
+++ server/core/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from flask import send_file
+from services.postgres import postgres
+from core.config.base_config import BaseConfig
 
 app = Flask(__name__)
+
+app.config.from_object(BaseConfig)
+
+postgres.app = app
+postgres.init_app(app)
 
 
 @app.route('/')
---
+++ server/core/config/base_config.py
@@ -0,0 +1,4 @@
+class BaseConfig():
+    SECRET_KEY = '123'
+    UPLOAD_FOLDER = '../../client/files'
+    SQLALCHEMY_DATABASE_URI = 'postgres://office_application:123@localhost:office_database'
