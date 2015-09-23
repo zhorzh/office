1dd0bf4bbf4138b9b06384e17d51519e2877e888
zhorzh <zhorzh.alexandr@gmail.com>
2015-09-23 19:31:22 -0400

    setup(project): add index html file

--- server/core/__init__.py
+++ server/core/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
+from flask import send_file
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home_page():
-    return('Home page under construction.')
+    return send_file('../../client/core/views/client.html')
